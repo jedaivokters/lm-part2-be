b1cd1ae52e4edcb1582b1b9ba3610d13a40e25d8
jedaivokters <allan.bernabe@gmail.com>
2023-11-15 09:32:01 +0800

    added cors whitelist to all modify graphql schema

--- src/main/java/com/example/crud/CrudApplication.java
+++ src/main/java/com/example/crud/CrudApplication.java
@@ -3,6 +3,12 @@ package com.example.crud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 @SpringBootApplication
 public class CrudApplication {
@@ -14,5 +20,23 @@ public class CrudApplication {
 	@Bean
 	public graphql.schema.GraphQLScalarType extendedScalarLong() {
 		return graphql.scalars.ExtendedScalars.GraphQLLong;
+	}
+
+	@Bean
+	public CorsFilter curionCorsConfiguration() {
+		CorsConfiguration corsConfiguration = new CorsConfiguration();
+		corsConfiguration.addAllowedOriginPattern("*");
+		corsConfiguration.addAllowedHeader("*");
+		corsConfiguration.addAllowedMethod("*");
+		corsConfiguration.addExposedHeader("ExposedHeaderName");
+
+		Map<String, CorsConfiguration> corsConfigurations = new LinkedHashMap<>(1);
+		corsConfigurations.put("/graphql/**", corsConfiguration);
+
+		UrlBasedCorsConfigurationSource configurationSource = new UrlBasedCorsConfigurationSource();
+		configurationSource.setCorsConfigurations(corsConfigurations);
+		configurationSource.setAllowInitLookupPath(true);
+
+		return new CorsFilter(configurationSource);
 	}
 }
